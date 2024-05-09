ef15d47a5f8a02a4b0a0780b66da3c5a5038a014
bart tynior <btynior@gmail.com>
2024-05-09 19:17:15 +0200

    Introduced authentication to the app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,40 @@
-export default function Home() {
+import { createClient } from "@/utils/supabase/server";
+import Link from "next/link";
+import SignOut from "./components/sign-out";
+
+export default async function Home() {
+  const supabase = createClient();
+  const session = await supabase.auth.getSession();
+  const user = session.data?.session?.user;
+
   return (
-    <>
-      <div></div>
-    </>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        <div>
+          Welcome, <strong>{user?.email ?? "Guest"}</strong>!
+        </div>
+
+        <div className="flex gap-2 text-sm mt-1">
+          {user ? <SignOut /> : <LinkButton href="/auth/sign-in">Login</LinkButton>}
+        </div>
+      </div>
+    </main>
   );
 }
+
+const LinkButton = ({
+  href,
+  children,
+}: {
+  href: string;
+  children: React.ReactNode;
+}) => {
+  return (
+    <Link
+      href={href}
+      className="text-black hover:text-black/30 transition duration-100 ease-in-out"
+    >
+      {children}
+    </Link>
+  );
+};
--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -1,7 +1,9 @@
 import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
-export const createClient = (cookieStore: ReturnType<typeof cookies>) => {
+export function createClient() {
+  const cookieStore = cookies();
+
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
@@ -13,22 +15,14 @@ export const createClient = (cookieStore: ReturnType<typeof cookies>) => {
         set(name: string, value: string, options: CookieOptions) {
           try {
             cookieStore.set({ name, value, ...options });
-          } catch (error) {
-            // The `set` method was called from a Server Component.
-            // This can be ignored if you have middleware refreshing
-            // user sessions.
-          }
+          } catch (error) {}
         },
         remove(name: string, options: CookieOptions) {
           try {
             cookieStore.set({ name, value: "", ...options });
-          } catch (error) {
-            // The `delete` method was called from a Server Component.
-            // This can be ignored if you have middleware refreshing
-            // user sessions.
-          }
+          } catch (error) {}
         },
       },
     }
   );
-};
+}
